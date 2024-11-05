cc2f5a7ac72faddfba423cb05eeea44511cf880d
Harjit <workandresearchbrar@gmail.com>
2024-11-05 09:02:46 +0000

    feat: add xgboost model

---
+++ experiment-tracking/model_xgboost_optuna.py
@@ -0,0 +1,71 @@
+import os
+
+import mlflow
+import optuna
+from xgboost import XGBRegressor
+
+from utils.df_process import preprocess
+from utils.feature_util import (
+    data_dict,
+    add_pudo_column,
+    feature_pipeline,
+    feature_selector,
+    setup_experiment,
+    mlflow_default_logging
+)
+
+setup_experiment()
+mlflow.xgboost.autolog()
+data = data_dict(debug=int(os.environ["DEBUG"]))
+
+def run(params: dict, pudo: int):
+    with mlflow.start_run():
+        # Preprocessing
+        train = data["train_data"]
+        valid = data["valid_data"]
+        X_train, y_train = preprocess(train, target=True, filter_target=True)
+        X_valid, y_valid = preprocess(valid, target=True, filter_target=True)
+
+        # Fit feature pipe
+        transforms = [add_pudo_column, feature_selector] if pudo else []
+        feature_pipe = feature_pipeline(transforms)
+        X_train = feature_pipe.fit_transform(X_train)
+        X_valid = feature_pipe.transform(X_valid)
+
+        # Fit model
+        model = XGBRegressor(early_stopping_rounds=50, **params)
+        model.fit(
+            X_train,
+            y_train,
+            eval_set=[(X_valid, y_valid)]
+        )
+
+        # Default mlflow logs
+        MODEL_TAG = "xgboost"
+        args = [model, MODEL_TAG, data, X_train, y_train, X_valid, y_valid]
+        logs = mlflow_default_logging(*args)
+
+        # Logging feature pipe, use pudo
+        mlflow.log_param("pudo", pudo)
+        mlflow.sklearn.log_model(feature_pipe, "feature_pipe")
+
+    return logs["rmse_valid"]
+
+def objective(trial):
+    params = {
+        "max_depth": trial.suggest_int("max_depth", 4, 100),
+        "n_estimators": trial.suggest_int("n_estimators", 1, 10, step=1) * 100,
+        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 1, log=True),
+        "min_child_weight": trial.suggest_float("min_child_weight", 0.1, 300, log=True),
+        "objective": "reg:squarederror",
+        "seed": 42,
+    }
+
+    return run(params, pudo=trial.suggest_categorical("pudo", [0, 1]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=int(sys.argv[1]))
